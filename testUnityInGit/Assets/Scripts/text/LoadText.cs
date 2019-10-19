ca87c5102a01560de5f318d0a18e9ed528b7ea40
henryZ <henrystriker8@gmail.com>
2019-10-19 22:52:14 +0800

    testRecvText01

---
+++ testUnityInGit/Assets/Scripts/text/LoadText.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+
+public class LoadText : MonoBehaviour
+{
+    string text01 = "";
+
+    void Start()
+    {
+        // TextAsset txt = Resources.Load("test01") as TextAsset;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            TextAsset txt = Resources.Load("test01") as TextAsset;
+            text01 = txt.text;
+            ShowTextMessage();
+        }
+    }
+
+    void ShowTextMessage()
+    {
+        Debug.Log("text01 message is: " + text01);
+    }
+}
